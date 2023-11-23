a950bfc41307a881afba7ff6b38fa8bc53203e7c
Emiliano Bassoli <emiliano.bassoli@vcube.it>
2023-11-23 14:49:40 +0100

    add python formatter

---
+++ plugins/black.lua
@@ -0,0 +1,74 @@
+-- mod-version:3
+
+-- Automatic formatter for python code
+-- Dependencies: `black` command in PATH or whatever formatter is chosen in
+-- configuration
+
+-- This is a slightly re-written version of https://git.sr.ht/~tmpod/black-lite
+
+-- Run with
+--   Black: format
+
+-- Imports
+local core = require "core"
+local command = require "core.command"
+local config = require "core.config"
+local common = require "core.common"
+
+-- Config
+config.plugins.black = common.merge({
+  enabled = true,
+  exec = "black",
+  args = "",
+  config_spec = {
+    name = "Python Code Formatter",
+    {
+      label = "Formatter command",
+      description = "Path to executable.",
+      path = "exec",
+      type = "string",
+      default = "black"
+    },
+    {
+      label = "Formatter arguments",
+      description = "Command line arguments.",
+      path = "args",
+      type = "string",
+      default = "",
+    }
+  }
+}, config.plugins.black)
+
+-- Main logic
+local function format()
+
+  -- Save the file if it is unsaved or dirty
+  local doc = core.active_view.doc
+  if doc:get_name() == "unsaved" or doc:is_dirty() then
+    doc:save()
+  end
+
+  -- Run command
+  local ok, _, code = os.execute(
+    string.format(
+      "%s %s %s",
+      config.plugins.black.exec,
+      config.plugins.black.args,
+      doc:get_name()
+    )
+  )
+
+  -- Return with error code if any
+  if not ok then
+    core.log("Failed to format file, return code is " .. code)
+    return
+  end
+
+  -- Log success and reload file
+  core.log("File is reformatted, reloading document")
+  doc:reload()
+end
+
+-- Register the command
+command.add("core.docview", {["black:format"] = format})
+
